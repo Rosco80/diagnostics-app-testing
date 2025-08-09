bf23ed85662558ad28a31cf2fe723eb5cbc1f229
Rosco80 <rossiandrea18@gmail.com>
2025-08-09 10:35:02 +0800

    Update app.py - gpt5 machine metadata display V7

--- app.py
+++ app.py
@@ -663,8 +663,9 @@ if uploaded_files and len(uploaded_files) == 3:
         if df is not None:
             discovered_config = auto_discover_configuration(files_content['source'], actual_curve_names)
             if discovered_config:
-                rpm = extract_rpm(files_content['levels'])
-                machine_id = discovered_config.get('machine_id', 'N/A')
+               st.session_state['auto_discover_config'] = discovered_config
+               rpm = extract_rpm(files_content['levels'])
+               machine_id = discovered_config.get('machine_id', 'N/A')
                 if st.session_state.active_session_id is None:
                     db_client.execute("INSERT INTO sessions (machine_id, rpm) VALUES (?, ?)", (machine_id, rpm))
                     st.session_state.active_session_id = get_last_row_id(db_client)
